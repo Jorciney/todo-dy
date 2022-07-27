a7aec0cc05ec5c93d98af8fa92d8ee9da0af61b9
Jorciney <jorciney.dias@hotmail.com>
2022-07-27 10:05:30 +0200

    feat(todo) added addButton

--- src/app/todo/todo.view.ts
+++ src/app/todo/todo.view.ts
@@ -7,22 +7,25 @@ import { TodoService } from '../todo.service';
   selector: 'app-todo-view',
   template: `
     <div class="todo-body">
-      <div class="box box-4">
-        <div class="box-content flexible">
-          <input type="text" class="todo-input-field" placeholder="Enter a todo" autofocus="" [(ngModel)]="todo.title"
-                 (keyup.enter)="addTodo()">
+      <div class="flexible-row">
+        <div class="box box-4">
+          <div class="box-content flexible">
+            <input type="text" class="todo-input-field" placeholder="Enter a todo" autofocus="" [(ngModel)]="todo.title"
+                   (keyup.enter)="addTodo()">
+          </div>
         </div>
+        <button class="add-button" role="button" (click)="addTodo()">+</button>
       </div>
-      <div>
-        <ul>
-          <li *ngFor="let todo of todoList | async" class="todo-list">
-            <div (mouseover)="todoComponent.isHovered = true" (mouseleave)="todoComponent.isHovered = false">
-              <app-todo [todo]="todo" #todoComponent (delete)="deleteTodo($event)"
-                        (updateTodo)="updateTodo($event)"></app-todo>
-            </div>
-          </li>
-        </ul>
-      </div>
+    </div>
+    <div>
+      <ul>
+        <li *ngFor="let todo of todoList | async" class="todo-list">
+          <div (mouseover)="todoComponent.isHovered = true" (mouseleave)="todoComponent.isHovered = false">
+            <app-todo [todo]="todo" #todoComponent (delete)="deleteTodo($event)"
+                      (updateTodo)="updateTodo($event)"></app-todo>
+          </div>
+        </li>
+      </ul>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
